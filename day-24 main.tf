9139d769c7bcfd561444acecb158c212068fb1aa
Christianaorji <daberechi731@gmail.com>
2024-10-26 00:14:31 +0000

    Update day-24 main.tf

--- day-24 main.tf
+++ day-24 main.tf
@@ -39,3 +39,109 @@ resource "aws_subnet" "sub1"{
     
   }
   
+resource "aws_security_group" "websg" {
+  name_prefix = "web"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+description = "SSH"
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+  }
+
+egress {
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+tags = {
+name = "web"
+}
+  }
+  
+resource "aws_s3_bucket" "example" {
+  bucket = "christianaterraform2024project"
+}
+
+
+resource "aws_instance" "webserver1" {
+  ami = "ami-08eb150f611ca277f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.websg.id]
+  subnet_id = aws_subnet.sub1.id
+  user_data = base64encode(file("userdata.sh"))
+}
+
+resource "aws_instance" "webserver2" {
+  ami = "ami-08eb150f611ca277f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.websg.id]
+  subnet_id = aws_subnet.sub2.id
+  user_data = base64encode(file("userdata1.sh"))
+  
+}
+
+#create alb
+resource "aws_lb" "myalb" {
+  name = "myalb"
+  internal = false
+  load_balancer_type = "application"
+
+  security_groups = [aws_security_group.websg.id]
+  subnets = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+
+  tags = {
+    name = "web"
+  }
+}
+  
+resource "aws_lb_target_group" "tg" {
+  name = "mytg"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.myvpc.id
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+  
+resource "aws_lb_target_group_attachment" "attach1" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver1.id
+  port = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach2" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver2.id
+  port = 80
+}
+
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.myalb.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.tg.arn
+    type = "forward"
+  }
+} 
+
+output "loadbalancerdns" {
+  value = aws_lb.myalb.dns_name
+}
